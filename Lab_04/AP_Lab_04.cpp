2f3385797e175b449fa580c6b10659dba031031f
Nadiya Fomenko <nadiya.fomenko@gmail.com>
2020-03-29 12:49:59 +0300

    Lab 04

---
+++ Lab_04/AP_Lab_04.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define LEN 100
+
+void findwords(char* s);
+void printWord(char* s, int k);
+
+int count = 0;
+int k = 0;
+
+int main() {
+	char s[LEN];
+	gets_s(s, LEN);
+	findwords(s);
+	printf("%d", count);
+}
+
+void findwords(char* s) {
+	if (*s != ' ' && *s != '\0') k++;
+	if (*s == '\0') {
+		if (*(s - 1) == *(s - k)) printWord(s - k, k);
+		count++;
+		return;
+		k = 0;
+	}
+	if (*s == ' ') {
+		if (*(s - 1) == *(s - k)) printWord(s - k, k);
+		count++;
+		k = 0;
+	}
+	findwords(s + 1);
+}
+
+void printWord(char* s, int k) {
+	for (int i = 0; i < k; i++) {
+		printf("%c", *(s + i));
+	}
+	puts("\t");
+}
