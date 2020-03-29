26f987ffb810ebb8341e5dd2038a19b5a575d8f3
Nadiya Fomenko <nadiya.fomenko@gmail.com>
2020-03-29 12:55:26 +0300

    Lab 06

---
+++ Lab_06/AP_Lab_06.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include "header.h"
+
+
+
+int main() {
+	int percent = 0;
+	city cities[10];
+	int count = setCities(cities, &percent);
+	InsertionSort(cities, count);
+	Percent(percent, count);
+}
