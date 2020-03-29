0226eef981cba61745da9dcb174445bb92f884e4
Nadiya Fomenko <nadiya.fomenko@gmail.com>
2020-03-29 12:48:33 +0300

    Lab 03

---
+++ Lab_03/AP_Lab_03.cpp
@@ -0,0 +1,144 @@
+﻿#include <stdio.h>
+
+//дод. завдання: вивести всі дії з main() у функції, глобальною має бути тільки структура +
+//організувати інший алгоритм сортування +
+
+//шаблон структури
+typedef struct Structcity {
+	char name[50];
+	int year;
+	char country[50];
+	int population;
+} city;
+
+
+
+//прототипи функцій
+void printCity(city* c);
+city setCity();
+int setCities(city* cities, int* percent);
+//void sortYear(city cities[10], int k);
+void Percent(int p, int c);
+void InsertionSort(city* cities, int k);
+
+
+
+
+int main() {
+	int percent = 0;
+	city cities[10];
+	int count = setCities(cities, &percent);
+	InsertionSort(cities, count);
+	Percent(percent, count);
+}
+
+
+
+
+
+
+
+int setCities(city*cities, int*percent) {
+	int counter = 0;
+	int i = 0;
+	int q = 0;
+	puts("Press enter no start:");
+	do {
+		city city1 = setCity();
+		cities[i] = city1;
+		counter++;
+		i++;
+		if (city1.year < 1200) {
+			(*percent)++;
+		}
+		puts("\nIf you want to add one more city press 1 (else 0):\n");
+		scanf_s("%d", &q);
+	} while (q == 1);
+
+	puts("\n\n\tTable:\n");
+	for (int i = 0; i < counter; i++) {
+		printCity(cities + i);
+	}
+
+	return counter;
+}
+
+city setCity() {
+
+	city c;
+	getchar();
+	puts("Enter city: ");
+
+	gets_s(c.name, 49);
+
+	puts("Enter county: ");
+	gets_s(c.country, 49);
+
+	puts("Enter year: ");
+	scanf_s("%d", &c.year);
+
+	puts("Enter population: ");
+	scanf_s("%d", &c.population);
+
+	return c;
+}
+
+void printCity(city* c) {
+
+	printf("%s		%s		%d		%d\n", c->country, c->name, c->year, c->population);
+}
+
+
+//bubble sort
+void sortYear(city *cities, int k)
+{
+
+	city c;
+	for (int j = k; j > 0; j--)
+	{
+		for (int i = k; i > 0; i--)
+		{
+			if (cities[i].year > cities[i - 1].year)
+			{
+				c = cities[i - 1];
+				cities[i - 1] = cities[i];
+				cities[i] = c;
+			}
+		}
+	}
+
+	puts("\n\n\tSorted Table:\n");
+	for (int i = 0; i < k; i++) {
+		printCity(cities + i);
+	}
+}
+//
+
+void Percent(int p, int c) {
+	printf("percentage of cities under 1200 - %d%%", (100 * p) / c);
+}
+
+//Insertion Sort
+void InsertionSort(city* cities, int k)
+{
+	int newElement, location;
+	city c;
+
+	for (int i = 1; i < k; i++)
+	{
+		c = cities[i];
+		location = i - 1;
+		while (location >= 0 && cities[location].year > c.year)
+		{
+			cities[location+1] = cities[location];
+			location = location - 1;
+		}
+		cities[location + 1] = c;
+	}
+
+	puts("\n\n\tSorted Table:\n");
+	for (int i = 0; i < k; i++) {
+		printCity(cities + i);
+	}
+}
+//
