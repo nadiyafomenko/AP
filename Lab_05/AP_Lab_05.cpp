acb22e0b9015dce254eafc75cae12a8724d5885e
Nadiya Fomenko <nadiya.fomenko@gmail.com>
2020-03-29 12:52:41 +0300

    Lab 05

---
+++ Lab_05/AP_Lab_05.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+//пототипи
+long Perevert(long x);
+void function(int k, ...);
+
+
+int main() {
+    function(5, 157757, 423444, 95739, 45678, 2345);
+}
+
+
+void function(int k, ...) {
+
+    int* pik = &k;
+
+    for (; k; k--) {
+        printf("%d\n", Perevert(*++pik));
+        }
+}
+
+
+long Perevert(long x)
+{
+    long y = 0;
+
+ while (x)
+    {
+        y = y * 10 + x % 10;
+        x /= 10;
+    }
+    return y;
+}
