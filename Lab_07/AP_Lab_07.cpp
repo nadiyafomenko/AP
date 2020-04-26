e4977bf1ad00c7b42c12edc130c604c559128036
Nadiya Fomenko <nadiya.fomenko@gmail.com>
2020-04-26 16:22:59 +0300

    Lab 07 changed

--- Lab_07/AP_Lab_07.cpp
+++ Lab_07/AP_Lab_07.cpp
@@ -6,8 +6,10 @@
 #define MAX 10
 #define LEN 20
 
-void validation(long* mas, int numb, long* validMas);
-int v = 0;
+int validation(long* mas, int numb, long* validMas);
+void TextFileWrite(FILE* file, long* validMas, int count);
+void BinaryFileWrite(FILE* file, long* mas);
+
 
 int main() {
      FILE*ftxt, *fbin;
@@ -15,44 +17,52 @@ int main() {
      long mas[MAX] = { 0 };
      long ValidatedMas[MAX] = { 0 };
 
-     srand(time(0));
-
      printf("enter a control number:\n");
      scanf_s("%d",&numb);
 
      fbin = fopen("D:\\randoms.bin", "wb+");
-
-     for (int i = 0; i < MAX; i++) {
-         mas[i] = rand() * rand();
-         fprintf(fbin, " %d  ", mas[i]); 
-         printf("%d\n", mas[i]);
-     }
-     printf("Binary file is ready...\n");
+     BinaryFileWrite(fbin, mas);
+     printf("D:\\randoms.bin is ready ...\n");
      fclose(fbin);
-    
+
      ftxt = fopen("D:\\randoms.txt", "w");
-     validation(mas, numb, ValidatedMas);
-     for (int i = 0; i < v; i++) {
-         fprintf(ftxt, "%d\n", ValidatedMas[i]);
-     }
-     
-     fclose(ftxt);
+     int count = validation(mas, numb, ValidatedMas);
+     TextFileWrite(ftxt, ValidatedMas, count);
+     printf("D:\\randoms.txt is ready ...");
 }
 
-void validation(long* mas, int numb, long* validMas) {
+
+void BinaryFileWrite(FILE* file, long* mas) {
+    srand(time(0));
+    for (int i = 0; i < MAX; i++) {
+        mas[i] = (rand() << 32) + rand();
+        fprintf(file, " %ld  ", mas[i]);
+        printf("%d\n", mas[i]);
+    }
+}
 
 
+void TextFileWrite(FILE* file, long* validMas, int count) {
+    for (int i = 0; i < count; i++) {
+        fprintf(file, "%ld\n", validMas[i]);
+    }
+    fclose(file);
+}
+
+
+int validation(long* mas, int numb, long* validMas) {
+    int count = 0;
         for (int i = 0; i < MAX; i++) {
             char str[LEN];
-            sprintf(str, "%d", mas[i]);
+            sprintf(str, "%ld", mas[i]);
             for (int j = 0; j < LEN; j++) {
                 if (str[j] == (numb + 48)) {
-                    validMas[v] = mas[i];
-                    v++;
+                    validMas[count] = mas[i];
+                    count++;
                     break;
                 }
             }
         }
-
+        return count;
 }
 
